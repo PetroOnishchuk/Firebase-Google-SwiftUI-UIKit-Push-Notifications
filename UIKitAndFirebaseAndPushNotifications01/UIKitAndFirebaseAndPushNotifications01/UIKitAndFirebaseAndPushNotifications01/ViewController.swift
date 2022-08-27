6a9b46d485499a95b64e78de02033b070c502f34
PetroOnishchuk <petroonishchuk@icloud.com>
2022-08-27 13:14:08 +0300

    Start a New Project UIKitAndFirebaseAndPushNotifications01

---
+++ UIKitAndFirebaseAndPushNotifications01/UIKitAndFirebaseAndPushNotifications01/UIKitAndFirebaseAndPushNotifications01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitAndFirebaseAndPushNotifications01
+//
+//  Created by Petro Onishchuk on 8/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
